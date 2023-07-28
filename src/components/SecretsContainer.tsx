1d677203dfbd1dd3ec87ca1721873e99da1f129e
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-07-27 23:15:06 -0600

    feat: add next and prev buttons for pagination

--- src/components/SecretsContainer.tsx
+++ src/components/SecretsContainer.tsx
@@ -1,12 +1,25 @@
+import { Icon } from '@iconify/react'
 import { useFetch } from 'hooks'
-import { FC, ReactElement, useState } from 'react'
+import { FC, ReactElement, useCallback, useState } from 'react'
 import { secretsService } from 'services'
 import { Secrets } from 'types/secrets'
 import { Card } from './Card'
 
 export const SecretsContainer: FC = (): ReactElement => {
-  const { data, setData } = useFetch<Secrets[]>()
+  const [current, setCurrent] = useState<number>(1)
+  const { data: secrets, setData } = useFetch<Secrets[]>(`api/secrets?page=${current}&pageSize=10`)
+  const { data: allSecrets } = useFetch<Secrets[]>('api/allsecrets')
   const [loader, setLoader] = useState<boolean>(false)
+
+  const handleNext = useCallback(() => {
+    if (!allSecrets) return
+    setCurrent((prev) => (prev === allSecrets?.length / allSecrets?.length + 1 ? 0 : prev + 1))
+  }, [allSecrets])
+
+  const handlePrev = useCallback(() => {
+    if (!allSecrets) return
+    setCurrent((prev) => (prev === 0 ? allSecrets?.length / allSecrets?.length + 1 : prev + 1))
+  }, [allSecrets])
 
   // TODO: Refactor these functions to be more DRY
   const likeFn = async (id: string, secret: Secrets) => {
@@ -59,12 +72,26 @@ export const SecretsContainer: FC = (): ReactElement => {
     }
   }
 
-  if (!data) return <h1>loading</h1>
+  if (!secrets) return <h1>loading</h1>
   return (
-    <section className='bg-[#141113] h-[calc(100vh-70px)] py-6'>
-      {data.map((secret, index) => (
+    <section className='bg-[#141113] min-h-[calc(100vh-70px)] py-6'>
+      {secrets.map((secret, index) => (
         <Card key={index} secret={secret} likeFn={likeFn} dislikeFn={dislikeFn} loader={loader} />
       ))}
+      <div className='flex justify-around w-2xl m-auto'>
+        <button
+          className='flex justify-center items-center h-[2rem] w-[12rem] border-none bg-[#f2508f] rounded-md'
+          onClick={handlePrev}
+        >
+          <Icon icon='ep:arrow-left-bold' fontSize={24} />
+        </button>
+        <button
+          className='flex justify-center items-center h-[2rem] w-[12rem] border-none bg-[#3C8CD4] rounded-md'
+          onClick={handleNext}
+        >
+          <Icon icon='ep:arrow-right-bold' fontSize={24} />
+        </button>
+      </div>
     </section>
   )
 }
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -2,22 +2,22 @@ import { useCallback, useEffect, useState } from 'react'
 
 const url = import.meta.env.VITE_API_URL
 
-export const useFetch = <T>() => {
+export const useFetch = <T>(endpoint: string) => {
   const [data, setData] = useState<T | null>(null)
 
   const getSecrets = useCallback(async () => {
     try {
-      const res = await fetch(`${url}api/secrets`)
+      const res = await fetch(`${url}${endpoint}`)
       const response: T = await res.json()
       setData(response)
     } catch (error) {
       console.error(error)
     }
-  }, [])
+  }, [endpoint])
 
   useEffect(() => {
     getSecrets()
-  }, [getSecrets])
+  }, [getSecrets, endpoint])
 
   return { data, setData }
 }
