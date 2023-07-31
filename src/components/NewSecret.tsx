55ba82f45672657eb752f6558cf771a444e34b87
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-07-30 18:25:47 -0600

    refactor: new secret component with secret validation class

--- src/components/NewSecret.tsx
+++ src/components/NewSecret.tsx
@@ -1,27 +1,21 @@
 import { Icon } from '@iconify/react'
-import { Dispatch, FC, FormEvent, ReactElement, SetStateAction, useReducer } from 'react'
-import { formReducer, initialState } from 'utils'
+import { Secret } from 'domain/secret/Secret'
+import { SecretEntity } from 'domain/types/Secret'
+import { Dispatch, FC, FormEvent, ReactElement, SetStateAction, useCallback, useState } from 'react'
 
 interface NewSecretProps {
   setSecretWindow: Dispatch<SetStateAction<boolean>>
 }
 
 const url = `${import.meta.env.VITE_API_URL}api/insert`
-const regex = /^(?:[a-zA-Z0-9]{1,7}(?:,|$)){1,3}$/
 
 export const NewSecret: FC<NewSecretProps> = ({ setSecretWindow }): ReactElement => {
-  const [state, dispatch] = useReducer(formReducer, initialState)
-  const requestBody = {
-    age: state.age,
-    tags: state.tags,
-    gender: state.gender,
-    secret: state.secret,
-    likes: state.likes,
-    dislikes: state.dislikes,
-  }
-
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+  const [data, setData] = useState<SecretEntity>({ age: 0, dislikes: 0, likes: 0, tags: [], secret: '', gender: 'man' })
+  const [tags, setTags] = useState<string>('')
+  
+  const handleSubmit = useCallback(async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+    const requestBody = new Secret(data).getDataForRequest()
     try {
       await fetch(url, {
         method: 'POST',
@@ -30,19 +24,19 @@ export const NewSecret: FC<NewSecretProps> = ({ setSecretWindow }): ReactElement
         },
         body: JSON.stringify(requestBody),
       })
-      dispatch({ type: 'reset' })
     } catch (error) {
       console.error(error)
     } finally {
+      setSecretWindow((prev) => !prev)
       location.reload()
     }
-  }
+  }, [data])
 
   return (
     <section className='h-screen w-full absolute bg-[#0B090Aba] backdrop-blur-2 top-0 flex justify-center items-center'>
       <form
         onSubmit={handleSubmit}
-        className='w-xl bg-[#0B090A] h-[22.4em] rounded-xl text-[#F5F3F4] p-5'
+        className='w-xl bg-[#0B090A] h-[22.4em] rounded-xl text-[#F5F3F4] p-5 flex flex-col gap-5'
       >
         <div className='flex pb-2 justify-between items-center pr-2 border-1 border-b-[#F5F3F4] border-b-solid'>
           <p className='text-3xl'>What secret would you tell us? 💡</p>
@@ -57,7 +51,7 @@ export const NewSecret: FC<NewSecretProps> = ({ setSecretWindow }): ReactElement
             />
           </button>
         </div>
-        <div className='font-600 pt-4 border-1 pb-4 border-b-[#F5F3F4] border-b-solid'>
+        <div className='font-600 border-1 pb-4 border-b-[#F5F3F4] border-b-solid'>
           <p className='inline'>I am </p>
           <input
             required
@@ -66,13 +60,13 @@ export const NewSecret: FC<NewSecretProps> = ({ setSecretWindow }): ReactElement
             min={12}
             type='number'
             placeholder='age'
-            onChange={(e) => dispatch({ type: 'setField', field: 'age', value: e.target.value })}
+            onChange={(e) => setData((prev) => ({ ...prev, age: Number(e.target.value)}))}
           />
           <p className='inline'> years old and I am a </p>
           <select
             required
             className='input'
-            onChange={(e) => dispatch({ type: 'setField', field: 'gender', value: e.target.value.toString() })}
+            onChange={(e) => setData((prev) => ({ ...prev, gender: e.target.value === 'man' ? 'man' : 'woman'}))}
           >
             <option defaultValue='gender' className='hidden'>
               gender
@@ -83,23 +77,34 @@ export const NewSecret: FC<NewSecretProps> = ({ setSecretWindow }): ReactElement
         </div>
         <textarea
           required
-          onChange={(e) => dispatch({ type: 'setField', field: 'secret', value: e.target.value.toString() })}
           maxLength={420}
-          className='w-full h-[10rem] text-[18px] rounded-xl border-none mt-5 bg-[#181818] text-[#F5F3F4] px-2 py-1 resize-none'
+          className='w-full h-[10rem] text-[18px] rounded-xl border-none bg-[#181818] text-[#F5F3F4] px-2 py-1 resize-none'
+          onChange={(e) => setData((prev) => ({ ...prev, secret: e.target.value}))}
         />
         <div className='flex justify-between border-none'>
-          <input
-            pattern={regex.source}
-            maxLength={35}
-            onChange={(e) =>
-              dispatch({ type: 'setField', field: 'tags', value: e.target.value.split(',') })
-            }
-            className='bg-[#181818] text-[#F5F3F4] px-4 rounded-1 border-none w-70 py-2'
-            placeholder='tags (separated by commas and max 3)'
-          />
+          <div className='flex justify-center gap-2'>
+            <input
+              maxLength={7}
+              className='bg-[#181818] text-[#F5F3F4] px-4 rounded-1 border-none w-70 py-2'
+              placeholder='tag'
+              value={tags}
+              onChange={(e) => {
+                setTags(e.target.value)
+              }}
+            />
+            <button
+              disabled={data.tags.length === 3}
+              className='flex justify-center items-center border-none rounded-md w-[3em] bg-red-5 duration-300 hover:bg-red-7'
+              onClick={(e) => {
+                setData((prev) => ({ ...prev, tags: [...prev.tags, tags] })), e.preventDefault(), setTags('')
+              }}
+            >
+              <Icon icon='tabler:plus' fontSize={24} />
+            </button>
+          </div>
           <button
             type='submit'
-            className='flex justify-center items-center border-none rounded-md w-[4em] bg-[#d1d1d1] hover:bg-[#F5F3F4]'
+            className='flex justify-center items-center border-none rounded-md w-[4em] bg-blue-5 duration-300 hover:bg-blue-7'
           >
             <Icon icon='tabler:send' fontSize={24}  />
           </button>
